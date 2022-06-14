0a1a45b8887888958aad2adf0e813d02e297a49f
ictus <ictus@ictusui-MacBookPro.local>
2022-06-14 14:38:34 +0900

    Make simple chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,24 @@
+const messageList = document.querySelector('ul')
+const messageForm = document.querySelector('form')
 const socket = new WebSocket(`ws://${window.location.host}`)
 
-socket.addEventListener('open',()=>{
+socket.addEventListener('open', () => {
   console.log('Connected to Server ✅')
 })
 
-socket.addEventListener('message',(message)=>{
+socket.addEventListener('message', (message) => {
   console.log('New Message: ', message.data, 'from the server')
 })
 
-socket.addEventListener('close', ()=>{
+socket.addEventListener('close', () => {
   console.log('Disconnected from the server ❌')
 })
 
-setTimeout(()=>{
-  socket.send('Hello from the Browser')
-},5000)
+function handleSubmit(event) {
+  event.preventDefault()
+  const input = messageForm.querySelector('input')
+  socket.send(input.value)
+  input.value = ''
+}
+
+messageForm.addEventListener('submit', handleSubmit)
--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ import express from 'express'
 const port = 3000
 const app = express()
 
-app.set('view engine','pug')
-app.set('views', __dirname+'/views')
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
 
 app.use('/public', express.static(__dirname + '/public'))
-app.get('/',(_,res)=>res.render('home'))
-app.get('/*',(_,res)=>res.redirect('/'))
+app.get('/', (_, res) => res.render('home'))
+app.get('/*', (_, res) => res.redirect('/'))
 
 const handleListening = () => console.log(`✅ Listening on http://localhost:${port}`)
 
@@ -18,16 +18,21 @@ const server = http.createServer(app)
 
 const wss = new WebSocket.Server({server})
 
+function onSocketClose() {
+  console.log('Disconnected from the Browser ❌')
+}
 
-wss.on('connection',(socket)=> {
+const sockets = []
+
+wss.on('connection', (socket) => {
+  sockets.push(socket)
   console.log('Connected to Browser ✅')
-  socket.on('close', ()=>{
-    console.log('Disconnected from the Browser ❌')
+  socket.on('close', onSocketClose)
+  socket.on('message', (message) => {
+    sockets.forEach((aSocket) => {
+      aSocket.send(message.toString('utf-8'))
+    })
   })
-  socket.on('message',(message)=>{
-    console.log(message.toString('utf8'))
-  })
-  socket.send('hello!')
 })
 
 server.listen(3000, handleListening)
