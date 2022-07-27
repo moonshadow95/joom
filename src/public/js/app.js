295ee1b89d41b0de9019d87dd11f8fb01ac1a233
ictus <last_enemy@naver.com>
2022-07-27 16:15:08 +0900

    Controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,26 @@ const socket = io()
 const myFace = document.getElementById('myFace')
 const muteBtn = document.getElementById('mute')
 const cameraBtn = document.getElementById('camera')
+const camerasSelect = document.getElementById('cameras')
 
 let myStream
-let muted = true
-let cameraOff = true
+let muted = false
+let cameraOff = false
+
+async function getCamera() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    const cameras = devices.filter(device => device.kind === 'videoinput')
+    cameras.forEach(camera => {
+      const option = document.createElement('option')
+      option.value = camera.deviceId
+      option.innerText = camera.label
+      camerasSelect.appendChild(option)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 async function getMedia() {
   try {
@@ -20,8 +36,8 @@ async function getMedia() {
   }
 }
 
-
 function handleMuteClick() {
+  myStream.getAudioTracks().forEach(track => track.enabled = !track.enabled)
   if (muted === true) {
     muted = false
     muteBtn.innerText = 'Mute'
@@ -32,6 +48,7 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+  myStream.getVideoTracks().forEach(track => track.enabled = !track.enabled)
   if (cameraOff === true) {
     cameraOff = false
     cameraBtn.innerText = 'Camera Off'
@@ -40,6 +57,9 @@ function handleCameraClick() {
     cameraBtn.innerText = 'Camera On'
   }
 }
+
+getMedia()
+getCamera()
 
 muteBtn.addEventListener('click', handleMuteClick)
 cameraBtn.addEventListener('click', handleCameraClick)
