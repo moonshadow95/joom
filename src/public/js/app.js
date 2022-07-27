b2b06a5fee466f3ca14d8b73453a511cc02259b7
ictus <last_enemy@naver.com>
2022-07-27 16:41:09 +0900

    Camera switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,14 @@ async function getCamera() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices()
     const cameras = devices.filter(device => device.kind === 'videoinput')
+    const currentCamera = myStream.getVideoTracks()[0]
     cameras.forEach(camera => {
       const option = document.createElement('option')
       option.value = camera.deviceId
       option.innerText = camera.label
+      if (currentCamera.label === camera.label) {
+        option.selected = true
+      }
       camerasSelect.appendChild(option)
     })
   } catch (e) {
@@ -24,17 +28,31 @@ async function getCamera() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: {
+      facingMode: 'user'
+    }
+  }
+  const cameraConstrains = {
+    audio: true,
+    video: {deviceId: {exact: deviceId}}
+  }
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true, video: true
-    })
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    )
     myFace.srcObject = myStream
-
+    if (!deviceId) {
+      await getCamera()
+    }
   } catch (e) {
     console.log(e)
   }
 }
+
+getMedia()
 
 function handleMuteClick() {
   myStream.getAudioTracks().forEach(track => track.enabled = !track.enabled)
@@ -58,8 +76,11 @@ function handleCameraClick() {
   }
 }
 
-getMedia()
-getCamera()
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value)
+}
 
 muteBtn.addEventListener('click', handleMuteClick)
 cameraBtn.addEventListener('click', handleCameraClick)
+camerasSelect.addEventListener('input', handleCameraChange)
+
